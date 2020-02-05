fd97f6905a5d411427f8370a4e7625860dcba4d6
David Eber <deber116@gmail.com>
2020-02-05 21:46:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,11 +12,13 @@ end
 def reduce(source, starting_value = nil)
   result = nil
   source_index = 0 
-  while source_index < source.count do 
-    if starting_value
+  if starting_value
       result = starting_value
+    else 
+      result = source[source_index]
       source_index = 1
     end
+  while source_index < source.count do 
     yield(result, source[source_index])
     source_index += 1
   end
