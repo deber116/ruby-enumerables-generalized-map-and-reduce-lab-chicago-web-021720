526d2e6a568ac6d1e76bff61e030966336594cf2
David Eber <deber116@gmail.com>
2020-02-05 21:44:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+
+def map(source)
+  result = []
+  source_index = 0 
+  while source_index < source.count do
+    result.push(yield(source[source_index]))
+    source_index += 1 
+  end
+  result
+end
+
+def reduce(source, starting_value = nil)
+  result = nil
+  source_index = 0 
+  while source_index < source.count do 
+    if starting_value
+      result = starting_value
+      source_index = 1
+    end
+    yield(result, source[source_index])
+    source_index += 1
+  end
+  result
+end
